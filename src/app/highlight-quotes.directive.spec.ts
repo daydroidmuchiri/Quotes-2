3ab168d55076394e21c4845f45b17046d8361282
daydroid <daydroidmuchiri@gmail.com>
2019-12-04 11:21:53 +0300

    Auto-generated commit

--- src/app/highlight-quotes.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightQuotesDirective } from './highlight-quotes.directive';
-
-describe('HighlightQuotesDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightQuotesDirective();
-    expect(directive).toBeTruthy();
-  });
-});
